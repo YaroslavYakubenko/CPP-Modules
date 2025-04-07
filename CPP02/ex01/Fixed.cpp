fa7d6f115155210a3e77cf190e5913df70b103bc
Yaroslav Yakubenko <yyakuben@c2r5s7.42wolfsburg.de>
2025-04-07 18:50:16 +0200

    fixed ex01

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -4,25 +4,28 @@ Fixed::Fixed() : _value(0) {
 	std::cout << "Default constructor called" << std::endl;
 }
 Fixed::Fixed(const int intVal) {
-	std::cout << "Int constructor called" << std::endl;
 	_value = intVal << _fractionBits;
+	std::cout << "Int constructor called" << std::endl;
 }
 Fixed::Fixed(const float floatVal) {
-	std::cout << "Float constructor called" << std::endl;
 	_value = roundf(floatVal * (1 << _fractionBits));
-}
-Fixed::Fixed(const Fixed& other) {
-	std::cout << "Copy constructor called" << std::endl;
-	_value = other._value;//уточнить этот момент
+	std::cout << "Float constructor called" << std::endl;
 }
 Fixed::~Fixed() {
 	std::cout << "Destructor called" << std::endl;
+}
+Fixed::Fixed(const Fixed& other) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
 }
 Fixed& Fixed::operator=(const Fixed& other) {
 	std::cout << "Copy assignment operator called" << std::endl;
 	if (this != &other)
 		this->_value = other.getRawBits();
 	return *this;
+}
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+	return os << fixed.toFloat();
 }
 int Fixed::getRawBits() const {
 	return _value;
@@ -35,8 +38,4 @@ float Fixed::toFloat() const {
 }
 int Fixed::toInt() const {
 	return _value >> _fractionBits;
-}
-std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
-	os << fixed.toFloat();
-	return os;
 }
